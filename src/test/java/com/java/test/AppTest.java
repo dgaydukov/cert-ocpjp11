75718080b38f52cc5aac4b82e6620924322131fa
Dmitriy Gaydukov <gaydukov89@gmail.com>
2025-07-08 14:31:35 +0400

    refactor tests

--- src/test/java/com/java/test/AppTest.java
+++ src/test/java/com/java/test/AppTest.java
@@ -2,43 +2,10 @@ package com.java.test;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 
 public class AppTest {
     @Test
-    public void testPerson(){
-        Clock clock = Mockito.mock(Clock.class);
-        Person person = new Person(clock);
-        Mockito.doAnswer(new Answer() {
-            private int count = 0;
-            public Object answer(InvocationOnMock invocation) {
-                return (long) ++count;
-            }
-        }).when(clock).getTimestamp();
-        Assertions.assertEquals("run at 1", person.run(), "run mismatch");
-        Assertions.assertEquals("swim at 2", person.swim(), "swim mismatch");
-        Assertions.assertEquals("dance at 3", person.dance(), "dance mismatch");
-    }
-}
-
-interface Clock {
-    long getTimestamp();
-}
-class Person {
-    private final Clock clock;
-    public Person(Clock clock) {
-        this.clock = clock;
-    }
-
-    public String run(){
-        return "run at " + clock.getTimestamp();
-    }
-    public String swim(){
-        return "swim at " + clock.getTimestamp();
-    }
-    public String dance(){
-        return "dance at " + clock.getTimestamp();
+    public void test(){
+        Assertions.assertEquals(1, 1);
     }
 }
