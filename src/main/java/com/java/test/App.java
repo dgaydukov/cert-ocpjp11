510a2f5ef0a04651955921f0de24e13df449f78f
Dmitriy Gaydukov <gaydukov89@gmail.com>
2025-08-13 11:21:37 +0400

    clean up

--- src/main/java/com/java/test/App.java
+++ src/main/java/com/java/test/App.java
@@ -1,10 +1,7 @@
 package com.java.test;
 
-
 public class App {
     public static void main(String[] args) {
-        int a = 1, b = 1;
-        Object obj = a == b ? 5.0 : "hello";
-        System.out.println(obj.getClass().getName());
+
     }
 }
